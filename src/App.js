dbcbb63ec252536a65d88122d6680446200d2f37
MondRider <Gabriel.aline.ga62@gmail.com>
2024-02-05 22:12:46 -0300

    Axios Api-Teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [catFact, setCatFact] = useState('');
+
+  const fetchCatFact = () => {
+    axios.get('https://catfact.ninja/fact').then((res) => {
+      setCatFact(res.data.fact);
+    });
+  }
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flexbox'>
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
     </div>
   );
 }
+
 
 export default App;
